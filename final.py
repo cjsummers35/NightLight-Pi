fb9d85534bf3c6053b8d233866a83fe6c5ab4334
cjsummers35 <100862017+cjsummers35@users.noreply.github.com>
2022-05-03 09:21:26 -0600

    Update final.py

--- final.py
+++ final.py
@@ -2,20 +2,23 @@
 
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD) # mode is in board so pin calls are pin numbers not gpio value.
 pin_to_circuit = 7
 import subprocess
 from gpiozero import LED
 from gpiozero import Button
 import sys
+
 led = LED(27)
 button = Button(17)
 
+# stop button funtion will end the program if the button is pushed.
+
 def stop_button() :
     GPIO.setwarnings(False)
-    GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+    GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_UP) # sets button to up at start of funtion.
     while True :
-        input_state = GPIO.input(11)
+        input_state = GPIO.input(11) # if button is down the input is false if up it is true.
         if input_state == False :
             sys.exit()
         else :
@@ -31,21 +34,21 @@ def rc_time(pin_to_circuit) :
     while (GPIO.input(pin_to_circuit) == GPIO.LOW) :
         count += 1
     if count > 3000 :
-        subprocess.run(['./test'])
+        subprocess.run(['./test']) # test is a bash script file on my system that prints date, time, and "it is dark"
         led.on()
     elif count < 2000 :
         led.off()
     count = count * 0
-    stop_button()
+    stop_button() # calls to button control function.
 
 def main() :
 
     loops = 0 
     while True :
         if loops == 0 :
-            button.wait_for_press()
+            button.wait_for_press() # program waits to start until the user hits the button.
             loops += 1
-            time.sleep(3)
+            time.sleep(3) # program needs to sleep on start push or the program will close from the stop button function.
         elif loops > 0  :
             rc_time(pin_to_circuit)
         
